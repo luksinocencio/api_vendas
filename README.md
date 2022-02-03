8b057ef7c9f129bf4483fbfec6dfcdd9bccc0de1
luksinocencio <luksinocencio@gmail.com>
2022-02-03 13:37:25 -0300

    *

---
+++ README.md
@@ -0,0 +1,13 @@
+# api_vendas
+
+```
+docker run --name postgres -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+```
+
+```
+yarn typeorm migration:run
+```
+
+```
+yarn dev
+```
---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,46 @@
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '@shared/errors/AppError';
+
+import Product from '../typeorm/entities/Product';
+import ProductsRepository from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    // const productExists = await productsRepository.findByName(name);
+
+    // if (productExists) {
+    //   throw new AppError('There is already one product with this name');
+    // }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;
