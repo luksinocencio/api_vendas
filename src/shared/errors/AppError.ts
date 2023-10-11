2efd24e077bd88bbeb68cdb3862a204c26675bb7
luksinocencio <luksinocencio@gmail.com>
2023-10-11 13:23:49 -0300

    chore: remove semicolon

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,12 +1,3 @@
-class AppError {
-  public readonly message: string
-
-  public readonly statusCode: number
-
-  constructor(message: string, statusCode = 400) {
-    this.message = message
-    this.statusCode = statusCode
-  }
-}
-
-export default AppError
+version https://git-lfs.github.com/spec/v1
+oid sha256:82a82f5e607af4e282230cc249e6a4affef0539385aa039b86fb52f68378f6d6
+size 232
